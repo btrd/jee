7c2135084c3f20893bd92b4be14f9522014609a4
Jean Bertrand <jeanbertrand.contact@gmail.com>
2016-11-25 11:07:37 +0100

    Request

--- week301/src/main/java/fr/eservices/soaring/model/Reservation.java
+++ week301/src/main/java/fr/eservices/soaring/model/Reservation.java
@@ -1,12 +1,14 @@
 package fr.eservices.soaring.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class Reservation {
+public class Reservation implements Serializable  {
 	@Id
 	@ManyToOne
     @JoinColumn(name = "pilote_id")
--- week301/src/main/java/fr/eservices/soaring/model/Secteur.java
+++ week301/src/main/java/fr/eservices/soaring/model/Secteur.java
@@ -1,12 +1,14 @@
 package fr.eservices.soaring.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
-public class Secteur {
+public class Secteur implements Serializable {
 	@Id
 	@ManyToOne
     @JoinColumn(name = "epreuve_id")
