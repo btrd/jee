f8da73a85896b10bafcd37d3a6211fc2f6e405d0
Jean Bertrand <jeanbertrand.contact@gmail.com>
2017-01-03 21:18:42 +0100

    Resol week402

--- week402/src/main/java/fr/eservices/week402/ctrl/HelloController.java
+++ week402/src/main/java/fr/eservices/week402/ctrl/HelloController.java
@@ -1,14 +1,21 @@
 package fr.eservices.week402.ctrl;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/hello")
 public class HelloController {
 
-	public String sayHello(
-			// Model model, 
-			String name) 
-	{
-		// model.addAttribute("message", "Hello " + name + " !");
-		return "/sample.jsp";
-	}
-	
-	
+  @RequestMapping(value="/world", method=RequestMethod.GET)
+  public String sayHello(
+      Model model, 
+      @RequestParam(value="name", required=false, defaultValue=" ")String name) 
+  {
+    model.addAttribute("message", "Hello " + name + " !");
+    return "sample";
+  }
 }
